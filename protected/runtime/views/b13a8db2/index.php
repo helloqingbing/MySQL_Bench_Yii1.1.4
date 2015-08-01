5645e567d74d27b38da7f20e6748f4d3643970c0
helloqingbing <liqingbing1987@163.com>
2015-08-01 14:44:02 +0800

    init docs

---
+++ protected/runtime/views/b13a8db2/index.php
@@ -0,0 +1,9 @@
+<?php /* source file: E:\ProgramEx\webapp\chive\protected\views/schema\index.php */ ?>
+<script type="text/javascript">
+$(document).ready(function() {
+	if(location.href.indexOf('#') == -1)
+	{
+		chive.goto('tables');
+	}
+});
+</script>
